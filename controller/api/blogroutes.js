d0e67c53dcfb9a48a9fc814aaf47d61130c3250e
Ross F <rfahan@gmail.com>
2023-08-09 22:57:34 -0600

    added pseudo code and file/directory structure

---
+++ controller/api/blogroutes.js
@@ -0,0 +1,8 @@
+// routes
+//
+// get all '/' (include user)
+// get one by id (include user) -- show comments
+// post to create blog
+// put update
+// delete blog
+// 
---
+++ controller/index.js
@@ -0,0 +1,9 @@
+// routes 
+//
+//login page
+//signup page
+//dashboard page -- requires auth
+//edit blog page -- requires auth
+//homepage 
+// blog details (include comments)
+// create post -- requires auth
