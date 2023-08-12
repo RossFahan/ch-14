1aaa9e468a6044a4bd6fd72cc7a332bc8925b32f
Ross F <rfahan@gmail.com>
2023-08-11 19:38:58 -0600

    completed commentRoutes.js

--- controller/api/commentroutes.js
+++ controller/api/commentroutes.js
@@ -1,13 +1,67 @@
 const router = require('express').Router();
+const withAuth = require('../../public/js/auth');
+const { Comment } = require('../../models');
 
-// Create a new comment
-router.post('/', (req, res) => {
+// Route for creating new comment
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      content: req.body.content,
+      blog_id: req.body.blog_id,
+      user_id: req.session.user_id
+    });
 
+    res.status(201).json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// Delete a comment
-router.delete('/:id', (req, res) => {
+// Route for updating a comment
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const updatedComment = await Comment.update(
+      {
+        content: req.body.content
+      },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id
+        }
+      }
+    );
 
+    if (!updatedComment[0]) {
+      res.status(404).json({ message: 'No comment found' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Comment updated' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Route for deleting a comment
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const deletedComment = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id
+      }
+    });
+
+    if (!deletedComment) {
+      res.status(404).json({ message: 'No comment found' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Comment deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
