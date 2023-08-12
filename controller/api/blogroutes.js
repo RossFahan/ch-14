7d3c92893a49c1a70e97a28c73bfeeb8e71e9605
Ross F <rfahan@gmail.com>
2023-08-11 18:21:58 -0600

    added getters for blogRoutes.js

--- controller/api/blogroutes.js
+++ controller/api/blogroutes.js
@@ -2,23 +2,39 @@ const router = require('express').Router();
 const { BlogPost } = require('../../models');
 
 router.get('/', async (req, res) => {
-  // Fetch and display all blog posts
+    try {
+        const blogs = await Blog.findAll();
+        res.status(200).json(blogs);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get('/:id', async (req, res) => {
-  // Fetch and display a specific blog post by ID
+  try {
+    const blog = await Blog.findByPk(req.params.id);
+    
+    if (!blog) {
+      res.status(404).json({ message: 'No blog post found with this id' });
+      return;
+    }
+
+    res.status(200).json(blog);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', async (req, res) => {
-  // Create a new blog post
+    // Create a new blog post
 });
 
 router.put('/:id', async (req, res) => {
-  // Update an existing blog post
+    // Update an existing blog post
 });
 
 router.delete('/:id', async (req, res) => {
-  // Delete a blog post
+    // Delete a blog post
 });
 
 module.exports = router;
