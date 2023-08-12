4fda25562b0206ea34deda653b3f7c5fb0f2fa51
Ross F <rfahan@gmail.com>
2023-08-11 20:02:41 -0600

    added userRoutes.js logic

--- controller/api/userroutes.js
+++ controller/api/userroutes.js
@@ -1,18 +1,57 @@
 const router = require('express').Router();
+const { User } = require('../../models');
+const bcrypt = require('bcrypt');
 
-// Register a new user
-router.post('/register', (req, res) => {
+// Route for signing up a new user
+router.post('/signup', async (req, res) => {
+  try {
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: await bcrypt.hash(req.body.password, 10)
+    });
 
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.logged_in = true;
+
+      res.status(201).json(newUser);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// Log in a user
-router.post('/login', (req, res) => {
+// Route for logging in a user
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
 
+    if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+      res.status(400).json({ message: 'Incorrect email or password' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+
+      res.status(200).json({ user, message: 'Successful login' });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-// Log out a user
+// Route for logging out a user
 router.post('/logout', (req, res) => {
-
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(400).end();
+  }
 });
 
 module.exports = router;
--- models/Blog.js
+++ models/Blog.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
+d 
 class Blog extends Model {}
 
 Blog.init(
